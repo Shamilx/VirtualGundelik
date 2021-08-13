d064d1d088b3a40f3c078e2cebf374b7d83cfc69
Omero4 <omer04118102@gmail.com>
2021-08-13 15:37:51 +0400

    Update3

--- app/src/main/java/com/shamil/virtualgundelik/MainActivity.java
+++ app/src/main/java/com/shamil/virtualgundelik/MainActivity.java
@@ -2,7 +2,9 @@ package com.shamil.virtualgundelik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Register_OnClick(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+
+        startActivity(intent);
     }
 }
