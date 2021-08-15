df0ab9af84b7d8f97cf173b8e5d3917996fa5caf
Omero4 <omer04118102@gmail.com>
2021-08-15 15:37:55 +0400

    Update5(Demo)

--- app/src/main/java/com/shamil/virtualgundelik/RegisterActivity.java
+++ app/src/main/java/com/shamil/virtualgundelik/RegisterActivity.java
@@ -74,6 +74,7 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+
     public static boolean validate(String emailStr) {
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
         return matcher.find();
