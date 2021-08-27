138c83625dbdc6c8296fbb2126c7bcf6e138f636
Omero4 <omer04118102@gmail.com>
2021-08-27 15:49:59 +0400

    Update6(demo)

--- app/src/main/java/com/shamil/virtualgundelik/SearchResult.java
+++ app/src/main/java/com/shamil/virtualgundelik/SearchResult.java
@@ -1,23 +1,34 @@
 package com.shamil.virtualgundelik;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.gson.Gson;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SearchResult#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import Models.CustomAdapter;
+import Models.ListViewLine;
+import Models.VirtualGundelikUser;
+
+
 public class SearchResult extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
@@ -25,15 +36,11 @@ public class SearchResult extends Fragment {
     private String mParam1;
     private String mParam2;
     private DocumentSnapshot _data;
-    private TextView loading;
-    private TextView user_name_result;
-    private TextView user_lastname_result;
-    private TextView user_email_result;
-    private TextView user_birth_day_result;
-    private Thread download;
+    private ImageButton _button;
 
     public SearchResult() {}
     public SearchResult(DocumentSnapshot data) {_data = data; }
+
     public static SearchResult newInstance(String param1, String param2) {
         SearchResult fragment = new SearchResult();
         Bundle args = new Bundle();
@@ -56,10 +63,34 @@ public class SearchResult extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        ListView listView = getView().findViewById(R.id.searchResultList);
+        List<ListViewLine> lines = new ArrayList<>();
 
+        lines.add(new ListViewLine(getString(R.string.edit_text_hint3), _data.getString("firstName"), x -> {
+        }));
 
+        lines.add(new ListViewLine(getString(R.string.edit_text_hint4), _data.getString("lastName"), x -> {
+        }));
 
+        lines.add(new ListViewLine(getString(R.string.edit_text_hint5), _data.getString("birthDate"), x -> {
+        }));
+
+        CustomAdapter adapter = new CustomAdapter(getActivity(),lines);
+
+        listView.setAdapter(adapter);
+
+        _button = getView().findViewById(R.id.backSearchButton);
+        _button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction fr = getActivity().getSupportFragmentManager().beginTransaction();
+                fr.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left);
+                fr.replace(R.id.flFragment, new SearchFragment());
+                fr.commit();
+            }
+        });
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
