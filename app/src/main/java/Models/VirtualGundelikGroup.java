f1b418a650ae877d3686c113aea236adb9966136
Shamilx <shamil.shamiyevx@gmail.com>
2021-08-28 16:41:28 +0400

    Update6(Demo)

---
+++ app/src/main/java/Models/VirtualGundelikGroup.java
@@ -0,0 +1,9 @@
+package Models;
+
+import java.util.List;
+
+public class VirtualGundelikGroup {
+    public String GroupName;
+    public String GroupDesc;
+    public List<VirtualGundelikUser> GroupMembers;
+}
---
+++ app/src/main/java/Models/VirtualGundelikHomework.java
@@ -0,0 +1,14 @@
+package Models;
+
+import android.media.Image;
+
+import java.util.List;
+
+public class VirtualGundelikHomework {
+    public String TaskName;
+    public String TaskDesc;
+
+    public Image TaskImage;
+    public List<Image> TaskImages;
+    public String TaskWithText;
+}
