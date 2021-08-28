4ee2139cbfe313ad7818e1e5f38723988a824f09
Shamilx <shamil.shamiyevx@gmail.com>
2021-08-28 22:56:01 +0400

    Update6(Demo)

--- app/src/main/java/com/shamil/virtualgundelik/AddGroupActivity.java
+++ app/src/main/java/com/shamil/virtualgundelik/AddGroupActivity.java
@@ -3,6 +3,10 @@ package com.shamil.virtualgundelik;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.android.material.button.MaterialButton;
 
 public class AddGroupActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class AddGroupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_group);
+
+        init();
+    }
+
+    private void init() {
+        ImageButton button = findViewById(R.id.AddGroupGoBack);
+
+        button.setOnClickListener(view -> {
+            finish();
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+
+        });
+
     }
 }
